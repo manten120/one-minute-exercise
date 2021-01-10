5753978dd94ca31ef04bceaf2a587956ad3384ed
stacru <hamaw8888@gmail.com>
2021-01-10 23:32:04 +0900

    メインページを作成

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
+const mainRouter = require('./routes/main');
 const usersRouter = require('./routes/users');
 
 const app = express();
@@ -20,6 +21,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/main', mainRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
