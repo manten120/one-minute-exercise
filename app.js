fa8448923295cc6ff63bcd28aa8d4b3d9c80bb75
stacru <hamaw8888@gmail.com>
2021-01-17 21:52:45 +0900

    Twitter認証を実装

--- app.js
+++ app.js
@@ -4,11 +4,40 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const helmet = require('helmet');
+/* Twitter認証 */
+const session = require('express-session');
+const passport = require('passport');
+const { Strategy } = require('passport-twitter');
+const config = require('./config');
+/* Twitter認証 ここまで */
 
 const indexRouter = require('./routes/index');
 const mainRouter = require('./routes/main');
 const usersRouter = require('./routes/users');
 const loginRouter = require('./routes/login');
+
+/* Twitter認証 */
+passport.use(
+  new Strategy(
+    {
+      consumerKey: config.twitter.consumerKey,
+      consumerSecret: config.twitter.consumerSecret,
+      callbackURL: config.twitter.callbackURL,
+    },
+    (token, tokenSecret, profile, cb) => {
+      process.nextTick(() => cb(null, profile));
+    }
+  )
+);
+
+passport.serializeUser((user, cb) => {
+  cb(null, user);
+});
+
+passport.deserializeUser((obj, cb) => {
+  cb(null, obj);
+});
+/* Twitter認証 ここまで */
 
 const app = express();
 app.use(helmet());
@@ -23,10 +52,36 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+/* Twitter認証 */
+app.use(session({ secret: '417cce55dcfcfaeb', resave: false, saveUninitialized: false }));
+app.use(passport.initialize());
+app.use(passport.session());
+/* Twitter認証 ここまで */
+
 app.use('/', indexRouter);
 app.use('/main', mainRouter);
 app.use('/users', usersRouter);
 app.use('/login', loginRouter);
+
+/* Twitter認証 */
+app.get('/login/twitter', passport.authenticate('twitter'));
+
+app.get('/oauth_callback', passport.authenticate('twitter', { failureRedirect: '/' }), (req, res) => {
+  res.redirect('/main');
+});
+
+app.get('/logout', (req, res) => {
+  req.logout();
+  res.redirect('/');
+});
+
+// function ensureAuthenticated(req, res, next) {
+//   if (req.isAuthenticated()) {
+//     return next();
+//   }
+//   res.redirect('/');
+// }
+/* Twitter認証 ここまで */
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
--- routes/main.js
+++ routes/main.js
@@ -8,7 +8,21 @@ const router = express.Router();
 /* GET main page. */
 router.get('/', (req, res) => {
   // myData = { name: '名前', icon 'アイコンのパス' }
-  const myData = req.cookies.mdOneMinEx;
+  let myData;
+
+  if (req.user) {
+    myData = {
+      // eslint-disable-next-line no-underscore-dangle
+      name: req.user._json.name,
+      // eslint-disable-next-line no-underscore-dangle
+      icon: req.user._json.profile_image_url_https,
+    };
+  } else if (req.cookies.mdOneMinEx) {
+    myData = req.cookies.mdOneMinEx;
+  } else {
+    res.redirect('/');
+  }
+
   res.render('main', { myData, menusSrc, stampsSrc, textAndColorPairs });
   res.end();
 });
