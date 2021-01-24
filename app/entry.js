707ddcbb533945ed681edcede29411acd51a0806
stacru <hamaw8888@gmail.com>
2021-01-25 02:01:20 +0900

    コメントを追記

--- app/entry.js
+++ app/entry.js
@@ -76,6 +76,8 @@ $(leftColumn).on('mouseup', '.user:not(.mine)', function () {
  * メンション
  */
 
+// dataSomeone = { name: "メンション相手の名前", icon: "メンション相手のアイコンのパス" }
+// メンションしないとき dataSomeone = undefined
 let dataSomeone;
 
 // メンション相手の名前を表示する
@@ -100,7 +102,7 @@ $(leftColumn).on('click', '.user:not(.mine)', function () {
 });
 
 $(document).on('click', (event) => {
-  // 他者のアイコンと吹き出し以外をクリックした時
+  // 他人のアイコンと吹き出し以外をクリックした時
   if (!$(event.target).closest('.fukidashi:not(.mine)').length && !$(event.target).closest('.user:not(.mine)').length) {
     dataSomeone = undefined;
     setMention(dataSomeone);
@@ -114,16 +116,20 @@ $(document).on('click', (event) => {
 // サーバーの IP アドレスに対して WebSocket 通信を開始するリクエストを送る
 const socket = io();
 
+// mainページにアクセスしたとき
 if ($('#modalLong').length) {
+  // socket.io イベント送信
   const randomMenus = $('#modalLong').data('random-menus');
   socket.emit('call npc on loading main page', { randomMenus });
 }
 
+// myData = { name: "自分の名前", icon: "自分のアイコンのパス" }
 const myData = $('body').data('mine');
 
 const BtnText = $('.btn-text');
 // eslint-disable-next-line func-names
 BtnText.on('click', function () {
+  // socket.io イベント送信
   const key = $(this).data('key');
   const emitData = {
     to: '',
@@ -136,6 +142,7 @@ BtnText.on('click', function () {
   socket.emit('post my text', emitData);
   socket.emit('call npc', { from: myData, type: 'text', key });
 
+  // 自分の投稿を表示
   const text = $(this).text();
   const myTemplate = $('#myPostTemplate').clone().removeAttr('id');
   myTemplate.find('.text-comment').text(text).show();
@@ -148,6 +155,7 @@ BtnText.on('click', function () {
 
 const BtnStamp = $('.btn-stamp');
 BtnStamp.on('click', function () {
+  // socket.io イベント送信
   const key = $(this).data('key');
   const emitData = {
     to: '',
@@ -160,6 +168,7 @@ BtnStamp.on('click', function () {
   socket.emit('post my stamp', emitData);
   socket.emit('call npc', { from: myData, type: 'stamp', key });
 
+  // 自分の投稿を表示
   const src = $(this).attr('src');
   const myTemplate = $('#myPostTemplate').clone().removeAttr('id');
   if (dataSomeone) {
@@ -177,6 +186,7 @@ const imgRandom = $('.img-random');
 
 // eslint-disable-next-line func-names
 imgMenus.on('click', function () {
+  // socket.io イベント送信
   const key = $(this).data('key');
   const emitData = {
     to: '',
@@ -185,6 +195,7 @@ imgMenus.on('click', function () {
   };
   socket.emit('post my menu', emitData);
 
+  // 自分の投稿を表示
   const src = $(this).attr('src');
   const myTemplate = $('#myPostTemplate').clone().removeAttr('id');
   myTemplate.find('.img-comment').attr('src', src).show();
@@ -192,8 +203,10 @@ imgMenus.on('click', function () {
   myTemplate.appendTo(leftColumn).fadeIn();
   autoScroll();
 
+  // 選択したエクササイズを右カラムに表示
   imgSelected.attr('src', src).show();
 
+  // 時間が余ったとき用のエクササイズをランダムに決定
   const randomMenus = $('#modalLong').data('random-menus');
   let randomKey;
   do {
@@ -205,13 +218,14 @@ imgMenus.on('click', function () {
   // プログレスバー(タイマーの残り時間を表す)を最大値まで伸ばす
   $('.progress-bar').css('width', '100%');
 
-  // プログレスバーが伸びきってからタイマーを開始する
+  // プログレスバーが伸びきってからタイマーを開始
   setTimeout(() => {
     timer();
   }, 1000);
 });
 
 socket.on('someone posts menu', (data) => {
+  // 他人の投稿を表示
   const template = $('#postTemplate').clone().removeAttr('id').data('someone', data.from);
   template.find('.icon').attr('src', data.from.icon);
   template.find('.name').text(data.from.name);
@@ -222,6 +236,7 @@ socket.on('someone posts menu', (data) => {
 });
 
 socket.on('someone posts stamp', (data) => {
+  // 他人の投稿を表示
   const template = $('#postTemplate').clone().removeAttr('id').data('someone', data.from);
   if (data.to) {
     template.find('.mention-comment').text(`${data.to.name}さん`);
@@ -240,6 +255,7 @@ socket.on('someone posts stamp', (data) => {
 });
 
 socket.on('someone posts text', (data) => {
+  // 他人の投稿を表示
   const template = $('#postTemplate').clone().removeAttr('id').data('someone', data.from);
   if (data.to) {
     template.find('.mention-comment').text(`${data.to.name}さん`);
@@ -271,7 +287,7 @@ rightColum.on('scroll', function () {
   }
 });
 
-// タブクリック時のアニメーション
+// タブをクリックしたときのアニメーション
 $('.nav-link').on('click', () => {
   if (rightColum.scrollTop() >= 57) {
     rightColum.animate({ scrollTop: 57 }, 300);
