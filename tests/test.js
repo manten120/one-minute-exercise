a7ab54b128414bf68faea053a64a504ec0632c2d
stacru <hamaw8888@gmail.com>
2021-01-28 17:02:35 +0900

    テストを作成

--- tests/test.js
+++ tests/test.js
@@ -11,9 +11,40 @@ describe('/', () => {
       .expect(200));
 });
 
+describe('/login', () => {
+  test('簡易ログインするとき、クッキーをセットし/mainへリダイレクトする', () =>
+    request
+      .agent(app)
+      .post('/login')
+      .send({ name: 'taro', icon: 'hoge.js' })
+      .expect('Set-Cookie', /mdOneMinEx=j%3A%7B%22name%22%3A%22taro%22%2C%22icon%22%3A%22hoge.js%22%7D;/)
+      .expect('Location', '/main')
+      .expect(302));
+});
+
 describe('/main', () => {
   test('未ログインの場合 /#login にリダイレクトされる', () =>
     request(app).get('/main').expect('Location', '/#login').expect(302));
+});
+
+describe('/main', () => {
+  // cookieの有効日数
+  const cookieExpireDays = 30;
+  // ミリ秒単位に換算
+  const cookieExpireDaysMs = cookieExpireDays * 24 * 60 * 60 * 1000;
+  const d = new Date();
+  const expires = new Date(d.setDate(d.getDate() + 30)).toGMTString();
+  const cookie = `mdOneMinEx=j%3A%7B%22name%22%3A%22taro%22%2C%22icon%22%3A%22hoge.js%22%7D; Max-Age=${
+    cookieExpireDaysMs / 1000
+  }; Path=/; Expires=${expires}`;
+
+  test('簡易ログインしているときユーザー名が表示される', () =>
+    request
+      .agent(app)
+      .get('/main')
+      .set('Cookie', [cookie])
+      .send()
+      .expect(/taroさん/));
 });
 
 describe('/main', () => {
