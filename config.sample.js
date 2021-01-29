0fa0b205d1597d7c87261faa8c4fccebd66543c3
stacru <hamaw8888@gmail.com>
2021-01-29 16:51:59 +0900

    config.jsのサンプルファイルを作成

---
+++ config.sample.js
@@ -0,0 +1,9 @@
+// config.jsのサンプル
+module.exports = {
+  twitter: {
+    consumerKey: 'Twitter API の API KEY',
+    consumerSecret: 'Twitter API の API SECRET KEY',
+    callbackURL: 'http://localhost:8000/oauth_callback',
+  },
+  ipAddress: 'http://localhost:8000',
+};
