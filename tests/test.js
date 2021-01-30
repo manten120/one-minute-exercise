0f8a38ead7dea13a27140ab8dee7aa0e7d09e018
stacru <hamaw8888@gmail.com>
2021-01-30 22:41:18 +0900

    ログイフォームでの不正に対する挙動のテスト

--- tests/test.js
+++ tests/test.js
@@ -16,9 +16,36 @@ describe('/login', () => {
     request
       .agent(app)
       .post('/login')
-      .send({ name: 'taro', icon: 'hoge.js' })
-      .expect('Set-Cookie', /mdOneMinEx=j%3A%7B%22name%22%3A%22taro%22%2C%22icon%22%3A%22hoge.js%22%7D;/)
+      .send({ name: 'taro', icon: 'images/icons/hoge.js' })
+      .expect(
+        'Set-Cookie',
+        /mdOneMinEx=j%3A%7B%22name%22%3A%22taro%22%2C%22icon%22%3A%22images%2Ficons%2Fhoge.js%22%7D;/
+      )
       .expect('Location', '/main')
+      .expect(302));
+
+  test('ログインフォームから送られた名前が0文字のとき/へリダイレクトする', () =>
+    request
+      .agent(app)
+      .post('/login')
+      .send({ name: 'tarotarotar', icon: 'images/icons/hoge.js' })
+      .expect('Location', '/')
+      .expect(302));
+
+  test('ログインフォームから送られた名前が11文字以上のとき/へリダイレクトする', () =>
+    request
+      .agent(app)
+      .post('/login')
+      .send({ name: 'tarotarotar', icon: 'images/icons/hoge.js' })
+      .expect('Location', '/')
+      .expect(302));
+
+  test('ログインフォームから送られたアイコン画像URLが不正なとき/へリダイレクトする', () =>
+    request
+      .agent(app)
+      .post('/login')
+      .send({ name: 'tarotarotar', icon: 'himages/icons/hoge.js' })
+      .expect('Location', '/')
       .expect(302));
 });
 
@@ -34,7 +61,7 @@ describe('/main', () => {
   const cookieExpireDaysMs = cookieExpireDays * 24 * 60 * 60 * 1000;
   const d = new Date();
   const expires = new Date(d.setDate(d.getDate() + 30)).toGMTString();
-  const cookie = `mdOneMinEx=j%3A%7B%22name%22%3A%22taro%22%2C%22icon%22%3A%22hoge.js%22%7D; Max-Age=${
+  const cookie = `mdOneMinEx=j%3A%7B%22name%22%3A%22taro%22%2C%22icon%22%3A%22images%2Ficons%2Fhoge.js%22%7D; Max-Age=${
     cookieExpireDaysMs / 1000
   }; Path=/; Expires=${expires}`;
 
